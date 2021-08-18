77525706ff5f54023057dd56361957ac1b03281e
adust09 <shoukitsuda@icloud.com>
2021-08-18 22:46:56 +0900

    add tailwindcss, but cannot work

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -21,8 +21,11 @@ export default class IndexPage extends React.Component {
 
   render() {
     return (
-      <h1>hello world!</h1>
+      <section className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-center flex-col">
+        <h1 className="mb-4 text-green-500 text-3xl">サンプル</h1>
+        <p className="mb-2 text-center"> uuuum </p>
+        <button className="btn-blue">Let's Start!!</button>
+      </section>
     )
   }
-
 }
