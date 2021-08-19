b2b892f4173c4590683d18f2f40864b83ff5c010
adust09 <shoukitsuda@icloud.com>
2021-08-20 02:04:18 +0900

    add Layout

--- client/src/pages/withbutton.jsx
+++ client/src/pages/withbutton.jsx
@@ -1,20 +1,21 @@
 import { ethers } from "ethers";
 import * as zksync from "zksync";
 
+
 export default function OutsideUsageExample() {
     const MNEMONIC = process.env.MNEMONIC
 
 
     const withdrawETH = async () => {
         const syncProvider = await zksync.getDefaultProvider("ropsten");
-        console.log("syncProvider=",syncProvider)
+        console.log("syncProvider=", syncProvider)
 
         const ethersProvider = ethers.getDefaultProvider("ropsten");
-        console.log("ethersProvider=" ,ethersProvider)
+        console.log("ethersProvider=", ethersProvider)
 
         // const ethWallet = ethers.Wallet.fromMnemonic(MNEMONIC).connect(ethersProvider);
         const ethWallet = ethers.Wallet.createRandom().connect(ethersProvider);
-        console.log("ethWalletAddress=",ethWallet.address);
+        console.log("ethWalletAddress=", ethWallet.address);
 
         const syncWallet = await zksync.Wallet.fromEthSigner(ethWallet, syncProvider);
         console.log("syncWalletAddress=", syncWallet.address);
@@ -22,18 +23,17 @@ export default function OutsideUsageExample() {
         const withdraw =
             await
                 syncWallet.withdrawFromSyncToEthereum({
-                    ethAddress: ethWallet.ethAddress,
+                    ethAddress: ethWallet.address,
                     token: "ETH",
                     amount: ethers.utils.parseEther("0.998"),
                 });
-                console.log("withdraw.txHash=",withdraw.txHash);
-                await withdraw.awaitVerifyReceipt();
+        console.log("withdraw.txHash=", withdraw.txHash);
+        await withdraw.awaitVerifyReceipt();
     }
 
     return (
+
         <div>
-            <div>
-            </div>
             <section className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-center flex-col">
                 <h1 className="mb-4 text-green-500 text-3xl">sample</h1>
                 <p className="mb-2 text-center"> zkSYnc </p>
