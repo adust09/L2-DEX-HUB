e97427e656c627626dbba90a64804212ddf2a102
adust09 <shoukitsuda@icloud.com>
2021-08-20 23:25:47 +0900

    add zkswapABI

--- client/pages/zkswap.jsx
+++ client/pages/zkswap.jsx
@@ -1,9 +1,14 @@
 import { Wallet, Contract, utils } from 'ethers'
+import Web3 from 'web3'
+import zkswapABI from '../src/zkswap.ABI.json'
 
 export default function zkswap() {
 
+    let ABI = Web3.eth.Contract(zkswapABI);
+
+        const wallet = new Wallet('0x1c1a49fea9a4ede1dc8e582639f498d41fa3c4a9e2ab2b9d740a4a3ec14e1cbf')
+
     const contract = new Contract('0x8ECa806Aecc86CE90Da803b080Ca4E3A9b8097ad', ABI, wallet)
-    const wallet = new Wallet('0x1c1a49fea9a4ede1dc8e582639f498d41fa3c4a9e2ab2b9d740a4a3ec14e1cbf')
 
     async function depositETH(amount) {
         const tx = await contract.depositETH(wallet.address, {
@@ -13,7 +18,7 @@ export default function zkswap() {
     }
 
     // deposit 1 ETH
-    depositETH('1').then(console.log)
+    depositETH('0.5').then(console.log)
     return (
         <div>
             <section className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-center flex-col">
@@ -24,4 +29,3 @@ export default function zkswap() {
         </div>
     )
 }
-s
