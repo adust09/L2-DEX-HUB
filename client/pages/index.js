4f2a02bfac6df0a19514b334484f73220d508047
adust09 <shoukitsuda@icloud.com>
2021-08-20 11:18:58 +0900

    Revert "#3/zk sync"

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,15 @@
 import React from 'react'
-import { eth } from '../../web3/provider'
-import withdrawButton from '../pages/components/zkSync/withdraw'
+import { eth } from '../web3/provider'
 
 export default class IndexPage extends React.Component {
 
-  async Metamask() {
+  async componentDidMount() {
     try {
       await ethereum.enable() // Prompt user to let our DApp access their addresses
-
       const addresses = await eth.getAccounts() // Get user's ETH addresses
       console.log(addresses)
-
       const balance = await eth.getBalance(addresses[0])
       console.log(balance)
-
     } catch (err) {
       console.error("User denied access to their ETH addresses!")
     }
@@ -21,11 +17,7 @@ export default class IndexPage extends React.Component {
 
   render() {
     return (
-      <section className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-center flex-col">
-        <h1 className="mb-4 text-green-500 text-3xl">sample</h1>
-        <p className="mb-2 text-center"> dog </p>
-        <button className="btn-blue" withdrawButton>Let's Start!!</button>
-      </section>
+      <h1>hello world!</h1>
     )
   }
 }
