27008e479b173cf0481ed0a6b311d3708713d712
adust09 <shoukitsuda@icloud.com>
2021-08-21 00:36:11 +0900

    how to set ABI?

--- client/pages/zkswap.jsx
+++ client/pages/zkswap.jsx
@@ -1,13 +1,15 @@
 import { Wallet, Contract, utils } from 'ethers'
 import Web3 from 'web3'
 import zkswapABI from '../src/zkswap.ABI.json'
+const Web3EthAbi = require('web3-eth-abi');
+
 
 export default function zkswap() {
 
-    let ABI = Web3.eth.Contract(zkswapABI);
+    //try2
+    let ABI = zkswapABI
 
-        const wallet = new Wallet('0x1c1a49fea9a4ede1dc8e582639f498d41fa3c4a9e2ab2b9d740a4a3ec14e1cbf')
-
+    const wallet = new Wallet('0x1c1a49fea9a4ede1dc8e582639f498d41fa3c4a9e2ab2b9d740a4a3ec14e1cbf')
     const contract = new Contract('0x8ECa806Aecc86CE90Da803b080Ca4E3A9b8097ad', ABI, wallet)
 
     async function depositETH(amount) {
@@ -17,8 +19,8 @@ export default function zkswap() {
         return tx
     }
 
-    // deposit 1 ETH
     depositETH('0.5').then(console.log)
+
     return (
         <div>
             <section className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-center flex-col">
