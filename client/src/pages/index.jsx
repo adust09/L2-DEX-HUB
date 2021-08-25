91f585e835aa3683ee2445c958015b433320a632
adust09 <shoukitsuda@icloud.com>
2021-08-25 21:59:10 +0900

    not check deposit

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -11,7 +11,7 @@ export default function OutsideUsageExample() {
   const ethersProvider = ethers.getDefaultProvider("ropsten");
   const ethWallet = ethers.Wallet.fromMnemonic(MNEMONIC).connect(ethersProvider);
 
-  async function withdrawETH() {
+  async function withdrawETH(amount) {
 
     const syncProvider = await zksync.getDefaultProvider("ropsten");
     const syncWallet = await zksync.Wallet.fromEthSigner(ethWallet, syncProvider);
@@ -21,17 +21,17 @@ export default function OutsideUsageExample() {
         syncWallet.withdrawFromSyncToEthereum({
           ethAddress: ethWallet.address,
           token: "ETH",
-          amount: ethers.utils.parseEther("0.001"),
+          amount: ethers.utils.parseEther(amount),
         });
   }
 
 
-  async function depositETH() {
+  async function depositETH(amount) {
     let ABI = zkswapABI;
     let ZKSwapContract = '0x010254cd670aCbb632A1c23a26Abe570Ab2Bc467'
     const contract = new Contract(ZKSwapContract, ABI, ethWallet)
     const tx = await contract.depositETH(ethWallet.address, {
-      value: utils.parseEther('0.1')
+      value: utils.parseEther(amount)
     })
     return tx
   }
@@ -40,8 +40,9 @@ export default function OutsideUsageExample() {
     event.preventDefault();
     console.log("hello1");
     console.log("amount = ", event.target.amount.value);
-    withdrawETH();
-    depositETH();
+    const amount = event.target.amount.value
+    withdrawETH(amount);
+    depositETH(amount);
   }
 
 
